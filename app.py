5113368dce51e3c90757b06283acee9fd9385792
agroline96 <agroline.ferreteria@gmail.com>
2026-06-17 18:50:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown("""
 }
 @media (max-width: 768px) {
     .banner-agroline {
-        height: 14px !important;
+        height: 140px !important;
         background-size: contain !important;
         background-repeat: no-repeat !important;
         background-position: center !important;
